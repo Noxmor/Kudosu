2f42febb6033645d2928d70f1da01636888329e9
Noxmor <janek.drews@t-online.de>
2022-09-01 20:41:20 +0200

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "Kudosu"
+	architecture "x86_64"
+	startproject "Kudosu"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "Kudosu"
